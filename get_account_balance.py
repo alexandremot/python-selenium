b2cde8240e5b18f769c90dc2826f105f152e5fe8
Alexandre Mota <alexandremot@gmail.com>
2020-06-11 02:04:58 -0300

    incluido .env; primeira versao funcional

---
+++ get_account_balance.py
@@ -0,0 +1,66 @@
+
+import os
+import time
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+load_dotenv()
+
+dados_conta = {'agencia': os.getenv("AGENCIA"), 'conta': os.getenv("CONTA")}
+senha = os.getenv("SENHA")
+
+
+def my_func():
+    driver = webdriver.Chrome()
+    driver.get('https://www.itau.com.br/')
+
+    time.sleep(5)
+
+    try:
+        assert "Banco Itaú | Tudo pra você" in driver.title
+        print("ok!")
+
+        for dado in dados_conta.keys():
+            text_box_input = driver.find_element_by_id(dado)
+            text_box_input.click()
+            text_box_input.send_keys(dados_conta[dado])
+
+        time.sleep(1)
+
+        actions = ActionChains(driver).send_keys(Keys.TAB)
+        driver.find_element_by_id('btnLoginSubmit').click()
+
+        WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
+                (By.XPATH, '//div[@class="teclado clearfix"]'))
+            )
+
+        for item in senha:
+            a = driver.find_element(By.XPATH,
+                                    '//a[contains(text(), "' + item + '")]')
+            a.click()
+            time.sleep(1)
+
+        driver.find_element_by_id('acessar').click()
+
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+            (By.XPATH, '//a[@class="warningView"]'))
+            )
+
+        webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+
+        quadro_saldo = driver.find_element_by_id('tabSaldoExtratoDaConta')
+
+        print(quadro_saldo.text)
+
+    except AssertionError:
+        print("error!")
+
+
+if __name__ == "__main__":
+    my_func()
